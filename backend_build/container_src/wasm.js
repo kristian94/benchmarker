37514c0d9a3ea3d9e31ee228594d6c1358a6ad00
kristian <kristian.o.g.nielsen@gmail.com>
2021-02-24 11:48:03 +0100

    removed builded files from repo

--- backend_build/container_src/wasm.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const fs = require('fs').promises;
-const getWasmExports = fileName => fs.readFile(fileName)
-    .then(WebAssembly.instantiate)
-    .then(x => x.instance.exports);
-module.exports = {
-    getWasmExports
-};
--- backend_build/lib/utils.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const test = require('ava');
-const { formatNumber, convertPath } = require('./utils');
-test('convertPath - windows to posix', t => {
-    const winPath = 'C:\\Users\\Bob\\readme.md';
-    const posixPath = 'C:/Users/Bob/readme.md';
-    t.is(convertPath(winPath, '/'), posixPath);
-});
