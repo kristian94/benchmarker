b7c643c86d3d0fc74f37e974ff625d7b9e112c5f
kristian <kristian.o.g.nielsen@gmail.com>
2021-02-24 14:31:47 +0100

    deleted duplicated files

--- backend/src/container_src/wasm.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.getWasmExports = void 0;
-var fs = require('fs').promises;
-var getWasmExports = function (fileName) { return fs.readFile(fileName)
-    .then(WebAssembly.instantiate)
-    .then(function (x) { return x.instance.exports; }); };
-exports.getWasmExports = getWasmExports;
--- backend/src/lib/utils.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var test = require('ava');
-var _a = require('./utils'), formatNumber = _a.formatNumber, convertPath = _a.convertPath;
-test('convertPath - windows to posix', function (t) {
-    var winPath = 'C:\\Users\\Bob\\readme.md';
-    var posixPath = 'C:/Users/Bob/readme.md';
-    t.is(convertPath(winPath, '/'), posixPath);
-});
