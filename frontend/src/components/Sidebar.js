9d1b919e45b4537806ba3db61b92a09dd67006c3
Kristoffer Noga <k.noga@hotmail.com>
2021-03-30 09:49:46 +0200

    Fixed sidebar

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -7,21 +7,22 @@ function Sidebar(props) {
     return (
         <div className="flex-none w-52 px-3 pt-4 dark:bg-gray-900">
             <div className="text-center">
-                <h1 className="font-bold mb-6 text-2xl">Benchmarker</h1>
+                <h1 className="font-bold mb-3 text-2xl">Benchmarker</h1>
             </div>
-            <div className="grid justify-items-stretch grid-cols-1 gap-1">
+            <hr className="mb-3" />
+            <div className="grid grid-cols-1 gap-1">
                 {/* // TODO: Fix centering of text. Below is crap: */}
                 <div
-                    className={`border border-gray-900 text-sm font-bold h-10 text-center rounded-lg cursor-pointer hover:border-gray-600 transition ease-out duration-300 transform hover:scale-110 ${section === 1 ? "dark:bg-gray-700 bg-opacity-50" : ""}`}
+                    className={`border border-gray-900 text-sm font-bold h-10 flex items-center justify-center rounded-lg cursor-pointer hover:border-gray-600 transition ease-out duration-300 transform hover:scale-105 ${section === 1 ? "dark:bg-gray-700 bg-opacity-50" : ""}`}
                     onClick={_ => { setSection(1); history.push({ pathname: "/" }) }}
                 >
-                    <div className="align-middle">Tool</div>
+                    Tool
                 </div>
                 <div
-                    className={`border border-gray-900 text-sm font-bold h-10 text-center rounded-lg cursor-pointer hover:border-gray-600 transition ease-out duration-300 transform hover:scale-110 ${section === 2 ? "dark:bg-gray-700 bg-opacity-50" : ""}`}
+                    className={`border border-gray-900 text-sm font-bold h-10 flex items-center justify-center rounded-lg cursor-pointer hover:border-gray-600 transition ease-out duration-300 transform hover:scale-105 ${section === 2 ? "dark:bg-gray-700 bg-opacity-50" : ""}`}
                     onClick={_ => { setSection(2); history.push({ pathname: "/scenarios" }) }}
                 >
-                    <div className="align-middle">Scenarios</div>
+                    Scenarios
                 </div>
             </div>
         </div>
